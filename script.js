5379fa8505a2c59864bf186ebe9c9b3bd1977430
Abdu alyousef <94233680+Abdu-alyousef@users.noreply.github.com>
2023-07-10 02:56:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
 
-    // 2. Check if player's score is >= 20
-    if (scores[activePlayer] >= 20) {
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
